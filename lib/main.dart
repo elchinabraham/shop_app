76292db8827f95b128d6bda8ff4a9412047163cc
Elchin Abraham <elchinibrahimli8@gmail.com>
2022-04-07 11:48:54 +0400

    2022.04.07 11:48

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,11 @@ import 'package:provider/provider.dart';
 import 'package:shop_app/providers/cart.dart';
 import 'package:shop_app/providers/orders.dart';
 import 'package:shop_app/screens/cart_screen.dart';
+import 'package:shop_app/screens/edit_product_screen.dart';
 import 'package:shop_app/screens/orders_screen.dart';
 import 'package:shop_app/screens/product_detail_screen.dart';
 import 'package:shop_app/screens/product_overview_screen.dart';
+import 'package:shop_app/screens/user_products_screen.dart';
 import './providers/products.dart';
 
 void main() => runApp(MyApp());
@@ -39,6 +41,8 @@ class MyApp extends StatelessWidget {
           ProductDetailScreen.routedName: (ctx) => ProductDetailScreen(),
           CartScreen.routedName: (ctx) => const CartScreen(),
           OrdersScreen.routedName: (ctx) => OrdersScreen(),
+          UserProductsScreen.routeName: (ctx) => UserProductsScreen(),
+          EditProductScreen.routedName: (ctx) => EditProductScreen(),
         },
       ),
     );
---
+++ lib/widgets/user_product_item.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app/screens/edit_product_screen.dart';
+
+class UserProductItem extends StatelessWidget {
+  final String title;
+  final String imageUrl;
+
+  UserProductItem(this.title, this.imageUrl);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(title),
+      leading: CircleAvatar(
+        backgroundImage: NetworkImage(imageUrl),
+      ),
+      trailing: Container(
+        width: 100,
+        child: Row(
+          children: <Widget>[
+            IconButton(
+              onPressed: () {
+                Navigator.of(context).pushNamed(EditProductScreen.routedName);
+              },
+              icon: const Icon(
+                Icons.edit,
+              ),
+              color: Theme.of(context).primaryColor,
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.delete,
+              ),
+              color: Theme.of(context).errorColor,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
