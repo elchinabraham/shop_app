9a368e4448bad64762e5004d9bbee8b38662fb28
Elchin Abraham <elchinibrahimli8@gmail.com>
2022-04-26 17:06:56 +0400

    2022.04.26 17:003

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ import 'package:shop_app/screens/edit_product_screen.dart';
 import 'package:shop_app/screens/orders_screen.dart';
 import 'package:shop_app/screens/product_detail_screen.dart';
 import 'package:shop_app/screens/product_overview_screen.dart';
+import 'package:shop_app/screens/splash_screen.dart';
 import 'package:shop_app/screens/user_products_screen.dart';
 import './providers/products.dart';
 
@@ -33,7 +34,7 @@ class MyApp extends StatelessWidget {
             Provider.of<Auth>(ctx, listen: false).userId as String,
           ),
           update: (ctx, auth, previousProducts) => Products(
-            auth.token as String,
+            auth.token ?? "" as String,
             auth.userId as String,
           ),
         ),
@@ -50,7 +51,7 @@ class MyApp extends StatelessWidget {
             Provider.of<Auth>(ctx, listen: false).userId as String,
           ),
           update: (ctx, auth, previousProducts) => Orders(
-            auth.token as String,
+            auth.token ?? "" as String,
             auth.userId as String,
           ),
         ),
@@ -64,7 +65,16 @@ class MyApp extends StatelessWidget {
             colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.purple)
                 .copyWith(secondary: Colors.deepOrange),
           ),
-          home: auth.isAuth ? ProductOverviewScreen() : AuthScreen(),
+          home: auth.isAuth
+              ? ProductOverviewScreen()
+              : FutureBuilder(
+                  future: auth.tryAutoLogin(),
+                  builder: (ctx, authResultSnapshot) =>
+                      authResultSnapshot.connectionState ==
+                              ConnectionState.waiting
+                          ? SplashScreen()
+                          : AuthScreen(),
+                ),
           routes: {
             ProductDetailScreen.routedName: (ctx) => ProductDetailScreen(),
             CartScreen.routedName: (ctx) => const CartScreen(),
--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -1,13 +1,16 @@
+import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 import '../models/http_exception.dart';
 
 class Auth with ChangeNotifier {
-  String _token = "";
-  DateTime _expiryDate = DateTime(2022);
-  String _userId = "";
+  String? _token;
+  DateTime? _expiryDate;
+  String? _userId;
+  Timer? _authTimer;
 
   bool get isAuth {
     return token != null;
@@ -15,7 +18,7 @@ class Auth with ChangeNotifier {
 
   String? get token {
     if (_expiryDate != null &&
-        _expiryDate.isAfter(DateTime.now()) &&
+        (_expiryDate as DateTime).isAfter(DateTime.now()) &&
         _token != null) {
       return _token;
     }
@@ -23,7 +26,7 @@ class Auth with ChangeNotifier {
   }
 
   String get userId {
-    return _userId;
+    return _userId ?? "";
   }
 
   Future<void> _authenticate(
@@ -52,7 +55,18 @@ class Auth with ChangeNotifier {
       _userId = responseData['localId'];
       _expiryDate = DateTime.now()
           .add(Duration(seconds: int.parse(responseData['expiresIn'])));
+      _autoLogout();
       notifyListeners();
+
+      final prefs = await SharedPreferences.getInstance();
+      final userData = json.encode(
+        {
+          'token': _token,
+          'userId': _userId,
+          'expiryDate': _expiryDate!.toIso8601String(),
+        },
+      );
+      prefs.setString('userData', userData);
     } catch (error) {
       throw error;
     }
@@ -63,7 +77,47 @@ class Auth with ChangeNotifier {
     return _authenticate(email, password, 'signUp');
   }
 
+  Future<bool> tryAutoLogin() async {
+    final prefs = await SharedPreferences.getInstance();
+    if (!prefs.containsKey('userData')) {
+      return false;
+    }
+    final extractedData = prefs.getString('userData') as Map<String, Object>;
+    final expiryDate = DateTime.parse(extractedData['expiryDate'] as String);
+    if (expiryDate.isAfter(DateTime.now())) {
+      return false;
+    }
+    _token = extractedData['token'] as String;
+    _userId = extractedData['userId'] as String;
+    _expiryDate = expiryDate;
+    notifyListeners();
+    _autoLogout();
+    return true;
+  }
+
   Future<void> singin(String email, String password) async {
     return _authenticate(email, password, 'signInWithPassword');
+  }
+
+  Future<void> logout() async {
+    _token = null;
+    _userId = null;
+    _expiryDate = null;
+    if (_authTimer != null) {
+      _authTimer!.cancel();
+      _authTimer = null;
+    }
+    notifyListeners();
+    final prefs = await SharedPreferences.getInstance();
+    prefs.clear();
+  }
+
+  void _autoLogout() {
+    if (_authTimer != null) {
+      _authTimer!.cancel();
+    }
+
+    final timeToExpiry = _expiryDate!.difference(DateTime.now()).inSeconds;
+    _authTimer = Timer(Duration(seconds: timeToExpiry), logout);
   }
 }
